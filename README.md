51c2c00d945df0f784f59eb267b4cd5b41781ce8
Ülle Püttsepp <ulle.puttsepp@khk.ee>
2018-09-19 19:31:27 +0300

    Use Twig for templating, create a url for creating bookings

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Built with
+
+* [Silex](http://silex.sensiolabs.org/) - The web framework used
+* [Composer](https://getcomposer.org/) - Dependency Management
+* [Twig](http://twig.sensiolabs.org/) - Php template engine
--- web/index.php
+++ web/index.php
@@ -9,8 +9,11 @@ $app = new Silex\Application();
 //to turn on the debug mode
 $app['debug'] = true;
 
-$app->get('/hello', function() {
-	return 'Hello world!';
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+	'twig.path' => __DIR__.'/../views',
+]);
+$app->get('/bookings/create', function () use ($app) {
+	return $app['twig']->render('base.html.twig');
 });
 
 $app->run();
