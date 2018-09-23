4cf835f9f8415f27a1170593c6cb6f440264b6c7
Ülle Püttsepp <ulle.puttsepp@khk.ee>
2018-09-23 18:59:10 +0300

    Separate Application.php logic into more different methods

--- src/Application.php
+++ src/Application.php
@@ -14,22 +14,34 @@ class Application extends SilexApplication
     public function __construct(array $values = [])
     {
         parent::__construct($values);
-    
+
+        $this->configureServices();
+        $this->createDBTables();
+        $this->configureControllers();
+    }
+
+     /**
+    * Config app options and register services.
+    */
+    private function configureServices(){
         $this['debug'] = true;
-        
+
         $this->register(new TwigServiceProvider(), [
             'twig.path' => __DIR__.'/../views',
         ]);
 
-        // Database configuration
+         // Database configuration
         $this->register(new DoctrineServiceProvider(), [
             'db.options' => [
                 'driver' => 'pdo_sqlite',
                 'path' => __DIR__.'/../database/app.db',
             ],
         ]);
-        
-		//Creating a table if it doesn't exist yet
+    }
+	/**Creating all needed tables to database if don't exist
+    */
+    private function createDBTables(){
+        // Creating a table if it doesn't exist yet
 		if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
     		$this['db']->executeQuery("CREATE TABLE bookings (
         		id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -46,9 +58,14 @@ class Application extends SilexApplication
         		payingMethod VARCHAR(10) NOT NULL
     		);");
 		}
-		
+	}
+
+    /**
+    * Define all used routes and connect a route to its controller. */
+    private function configureControllers(){
 		$this->get('/bookings/create', function () {
 		return $this['twig']->render('base.html.twig');
 		});
-	}
+	}  
+
 }
