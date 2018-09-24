ab3ec086937741fc39e6c6bb0b6c8c8c03bd3c01
Ülle Püttsepp <ulle.puttsepp@khk.ee>
2018-09-24 18:49:44 +0300

    Save form data to the database if form is submitted

--- src/Application.php
+++ src/Application.php
@@ -87,8 +87,10 @@ class Application extends SilexApplication
         $this
             ->match('/bookings/create', new CreateBookingController(
                 $this['form.factory'],
-                $this['twig']
+                $this['twig'],
+                $this['db']
             ))
+            ->method('GET|POST')
         ;
 
 	}  
